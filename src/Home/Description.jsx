9b742aa3f5680a3698a0b0ac8bcfe3d1f2f04d1f
M1goNu <rifqi.aldino71@gmail.com>
2024-05-19 21:31:02 +0700

    add:hapus bg

--- src/Home/Description.jsx
+++ src/Home/Description.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Description() {
   return (
-    <div className="description-container rounded-lg p-8 bg-customRed">
+    <div className="description-container rounded-lg p-8">
       <h2 className="text-3xl font-bold mb-4 text-center">Discover the Beauty of Papua</h2>
       <p className="text-lg">Explore the stunning landscapes, rich cultural heritage, and diverse wildlife of Papua, Indonesia.</p>
     </div>
