f2de430b9d12336f9305c7b0c9747ab7da8a0c1f
M1goNu <rifqi.aldino71@gmail.com>
2024-05-20 12:33:39 +0700

    add:baru

--- src/Home/Map.jsx
+++ src/Home/Map.jsx
@@ -1,14 +1,14 @@
 import React, { useState, useEffect, useRef } from 'react';
 
 const slides = [
-  { href: "./public/Gambar/baru.jpg" },
-  { href: "./public/Gambar/map.jpg" },
-  { href: "./public/Gambar/2.jpg" },
-  { href: "./public/Gambar/papua1.jpg" }
+  "Gambar/baru.jpg",
+  "Gambar/map.jpg",
+  "Gambar/2.jpg",
+  "Gambar/papua1.jpg"
 ];
 
 function Map() {
-  const [currentSlide, setCurrentSlide] = useState(0); 
+  const [currentSlide, setCurrentSlide] = useState(1);
   const slideIntervalRef = useRef(null);
 
   useEffect(() => {
@@ -21,7 +21,7 @@ function Map() {
 
   const startSlideInterval = () => {
     slideIntervalRef.current = setInterval(() => {
-      setCurrentSlide((prevSlide) => (prevSlide + 1) % slides.length);
+      setCurrentSlide((prevSlide) => (prevSlide % slides.length) + 1);
     }, 3000);
   };
 
@@ -33,23 +33,23 @@ function Map() {
 
   return (
     <div className="carousel w-full relative">
-      {slides.map((slide, index) => (
+      {slides.map((image, index) => (
         <div
           key={index}
           id={`slide${index + 1}`}
-          className={`carousel-item relative w-full ${currentSlide === index ? 'block' : 'hidden'}`}
+          className={`carousel-item relative w-full ${currentSlide === index + 1 ? 'block' : 'hidden'}`}
         >
-          <img src={slide.href} className="w-full" alt={`Slide ${index + 1}`} />
+          <img src={image} className="w-full" alt={`Slide ${index + 1}`} />
           <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
             <button
               className="btn btn-circle"
-              onClick={() => handleSlideChange((index - 1 + slides.length) % slides.length)}
+              onClick={() => handleSlideChange(index === 0 ? slides.length : index)}
             >
               ❮
             </button>
             <button
               className="btn btn-circle"
-              onClick={() => handleSlideChange((index + 1) % slides.length)}
+              onClick={() => handleSlideChange(index === slides.length - 1 ? 1 : index + 2)}
             >
               ❯
             </button>
@@ -59,8 +59,8 @@ function Map() {
               {slides.map((_, dotIndex) => (
                 <div
                   key={dotIndex}
-                  onClick={() => handleSlideChange(dotIndex)}
-                  className={`h-4 w-4 rounded-full mx-1 cursor-pointer ${dotIndex === currentSlide ? 'bg-blue-500' : 'bg-gray-300'}`}
+                  onClick={() => handleSlideChange(dotIndex + 1)}
+                  className={`h-4 w-4 rounded-full mx-1 cursor-pointer ${dotIndex + 1 === currentSlide ? 'bg-blue-500' : 'bg-gray-300'}`}
                 ></div>
               ))}
             </div>
